6ad52727b034126a4a97a56915774506fad52b3a
serii burduja <seriiburduja@gmail.com>
2025-06-08 18:16:15 +0300

    feat: refactor coordinates

--- modules/coordinates.py
+++ modules/coordinates.py
@@ -8,6 +8,12 @@ from name_tuples.nt_coordinates import NtCoordinates
 
 def get_gps_coordinates() -> NtCoordinates:
     """Return gps coordinates using whereami by node js"""
+    output_lines = _get_whereami_output()
+    coordinates: NtCoordinates = _get_coordinates_from_list(output_lines)
+    return _round_coordinates(coordinates)
+
+
+def _get_whereami_output() -> list[str]:
     # Assuming 'whereami' is a command-line tool that returns GPS coordinates
     process = Popen(['whereami', '-f', 'json'], stdout=PIPE, stderr=PIPE)
     # Capture the output and error streams
@@ -18,9 +24,11 @@ def get_gps_coordinates() -> NtCoordinates:
         raise CantGetCoordinates(
             f"Error getting coordinates: {error.decode().strip()}")
 
-    output_lines = output.decode(errors='ignore').splitlines()
-    json_pattern = re.compile(r'{.*"latitude".*"longitude".*}')
+    return output.decode(errors='ignore').splitlines()
 
+
+def _get_coordinates_from_list(output_lines: list[str]) -> NtCoordinates:
+    json_pattern = re.compile(r'{.*"latitude".*"longitude".*}')
     for line in output_lines:
         match = json_pattern.search(line)
         if match:
@@ -38,3 +46,13 @@ def get_gps_coordinates() -> NtCoordinates:
             except json.JSONDecodeError as e:
                 raise CantGetCoordinates(f"Failed to decode JSON: {e}")
     raise CantGetCoordinates("No valid GPS coordinates found in output.")
+
+
+def _round_coordinates(coordinates: NtCoordinates):
+    """Round coordinates if USE_ROUNDED_COORDINATES is True."""
+    if USE_ROUNDED_COORDINATES:
+        return NtCoordinates(
+            latitude=round(coordinates.latitude, 1),
+            longitude=round(coordinates.longitude, 1)
+        )
+    return coordinates
